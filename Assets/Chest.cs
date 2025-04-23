ea1df3c7a5dcdf56ed356c36254e1bca46a59eb0
Yasmin <yasminbits@gmsil.com>
2025-04-23 18:49:21 +0300

    added code for chest and added a background for main menu

---
+++ Assets/Chest.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+public class Chest : MonoBehaviour, IInteractable
+{
+
+    public bool IsOpened {  get; private set; }
+    public string ChestID {  get; private set; }
+    public GameObject itemPrefab; // item that chest contains
+    public Sprite openedSprite;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        ChestID ??= GlobalHelper.GenerateUniqueID(gameObject); // unique id
+    }
+
+    public bool CanInteract()
+    {
+        return !IsOpened;
+    }
+
+    public void Interact()
+    {
+        if(!CanInteract()) return;
+
+        OpenChest();
+    }
+
+    public void OpenChest()
+    {
+        SetOpened(true);
+
+        //DropItem
+        if (itemPrefab != null)
+        {
+            GameObject dropedItem = Instantiate(itemPrefab, transform.position + Vector3.down, Quaternion.identity);
+        }
+
+    }
+
+    public void SetOpened(bool opened)
+    {
+        if (IsOpened = opened)
+        {
+            GetComponent<SpriteRenderer>().sprite = openedSprite;
+        }
+    }
+}
