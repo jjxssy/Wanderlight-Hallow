ba1972b2b3cb73cea81b73e28d49e60b02cca2ee
Sammis0 <samya.bahouth@gmail.com>
2025-06-19 14:36:57 +0300

    health-bar updates

---
+++ Assets/Scripts/Bar.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Bar : MonoBehaviour
+{
+    // [field:SerializeField]
+    // public int MaxValue(){get;private set;}
+    // [field:SerializeField]
+    // public int Value(){get;private set;}
+
+    // public void change()
+    // {
+    //     Value=Mathf.Clamp(Value+ amount , 0 , MaxValue);
+    // }
+    // private void Update()
+    // {
+    //     if(input.)
+    // }
+
+}
