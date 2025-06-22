1ebd9ee31c4e5a70a343997e0b5c4ea89b9dcbb3
Sammis0 <samya.bahouth@gmail.com>
2025-06-22 20:29:29 +0300

    you died screen

---
+++ Assets/Scripts/DeathManger.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class DeathManger : MonoBehaviour
+{   private static DeathManger instance;
+    public void Awake(){
+        if(DeathManger.instance==null)
+            this=instance;
+    }
+}
