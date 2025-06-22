d1fac23c79011e3b4f1486496a2ed64f10e0f208
Sammis0 <samya.bahouth@gmail.com>
2025-06-22 20:31:57 +0300

    Update DeathManger.cs

--- Assets/Scripts/DeathManger.cs
+++ Assets/Scripts/DeathManger.cs
@@ -2,8 +2,8 @@ using UnityEngine;
 
 public class DeathManger : MonoBehaviour
 {   private static DeathManger instance;
-    public void Awake(){
-        if(DeathManger.instance==null)
-            this=instance;
-    }
+//     public void Awake(){
+//         if(DeathManger.instance==null)
+//             this=instance;
+//     }
 }
